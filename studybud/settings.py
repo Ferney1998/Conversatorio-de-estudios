9a7061853e541f1bd1f624cb4bf2e7b44d4bcdbd
ferney <wcubides@unab.edu.co>
2022-08-10 18:26:17 -0500

    fixing issues

--- studybud/settings.py
+++ studybud/settings.py
@@ -90,10 +90,8 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 DATABASES = {
     'default': {
-        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-
     }
 }
 
